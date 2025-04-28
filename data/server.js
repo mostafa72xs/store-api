17f043726b9fcade2df8fbc1c1c41da6577401c3
mostafa72xs <119728038+mostafa72xs@users.noreply.github.com>
2025-04-28 04:17:15 +0300

    Update server.js

--- data/server.js
+++ data/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.json(
@@ -211,3 +211,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+module.exports = app;
