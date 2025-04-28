4b2e5a0f698ec32839dcd6b696010065b8500fd5
mostafa72xs <119728038+mostafa72xs@users.noreply.github.com>
2025-04-28 03:09:21 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get('/api/server', (req, res) => {
+app.get('/', (req, res) => {
   res.json(
     {message:{
         products:[
